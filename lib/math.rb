f046739ca14c4c0a1610e27b84419800f33e3fda
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2020-12-29 06:48:50 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,41 @@
 def addition(num1, num2)
 
+  sum = num1 + num2
+  p sum
+
 end
 
 def subtraction(num1, num2)
+
+  difference = num1 - num2
+  p difference
 
 end
 
 def division(num1, num2)
 
+  quotient = num1/num2
+  p quotient
+  
 end
 
 def multiplication(num1, num2)
-
+  
+  product = num1 * num2
+  p product
+  
 end
 
 def modulo(num1, num2)
 
+  modulus = num1%num2
+  p modulus
+  
 end
 
 def square_root(num)
 
+  root = Math.sqrt(num)
+  p root
+  
 end
